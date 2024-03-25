ed460a35a1c7a07e2467f6ad3d27b0b78f14a52f
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-03-25 18:01:00 -0300

    [fix] tailwind screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        'sm': '400px',
+        'sm': '100px',
         // => @media (min-width: 640px) { ... }
   
         'md': '768px',
